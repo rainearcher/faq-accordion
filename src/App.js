1e23e6272720ba12f1012b49170ab135d77a5b51
Raine Soriano <rainearcher@g.ucla.edu>
2023-12-31 02:30:52 -0800

    refactor question answers into array

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import starIcon from './icon-star.svg';
 import minusIcon from './icon-minus.svg';
 import plusIcon from './icon-plus.svg'
+import { useState } from 'react';
 
 function QuestionAnswer({question, answer, showAnswer}) {
   const icon = showAnswer ? minusIcon : plusIcon;
@@ -17,35 +18,46 @@ function QuestionAnswer({question, answer, showAnswer}) {
 }
 
 function App() {
+  const [questionAnswers, setQuestionAnswers] = useState([
+    {
+      question: "What is Frontend Mentor, and how will it help me?",
+      answer: `Frontend Mentor offers realistic coding challenges to help developers improve their
+      frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for
+      all levels and ideal for portfolio building.`,
+      showAnswer: true
+    },
+    {
+      question: "Is Frontend Mentor free?",
+      answer: `Yes, Frontend Mentor offers both free and premium coding challenges, with the free
+      option providing access to a range of projects suitable for all skill levels.`,
+      showAnswer: false
+    },
+    {
+      question: "Can I use Frontend Mentor projects in my portfolio?",
+      answer: `Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
+      way to showcase your skills to potential employers!`,
+      showAnswer: false
+    },
+    {
+      question: "How can I get help if I'm stuck on a Frontend Mentor challenge?",
+      answer: `The best place to get help is inside Frontend Mentor's Discord community. There's a help
+      channel where you can ask questions and seek support from other community members.`,
+      showAnswer: false
+    }
+  ]);
   return (
     <div className="App">
       <div className="card">
         <h1><img src={starIcon}/>FAQs</h1>
-        <QuestionAnswer
-          question="What is Frontend Mentor, and how will it help me?"
-          answer="Frontend Mentor offers realistic coding challenges to help developers improve their
-          frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for
-          all levels and ideal for portfolio building."
-          showAnswer={true}
-        />
-        <QuestionAnswer
-          question="Is Frontend Mentor free?"
-          answer="Yes, Frontend Mentor offers both free and premium coding challenges, with the free
-          option providing access to a range of projects suitable for all skill levels."
-          showAnswer={false}
-        />
-        <QuestionAnswer 
-          question="Can I use Frontend Mentor projects in my portfolio?"
-          answer="Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
-          way to showcase your skills to potential employers!"
-          showAnswer={false}
-        />
-        <QuestionAnswer
-          question="How can I get help if I'm stuck on a Frontend Mentor challenge?"
-          answer="The best place to get help is inside Frontend Mentor's Discord community. There's a help
-          channel where you can ask questions and seek support from other community members."
-          showAnswer={false}
-        />
+        {questionAnswers.map(({question, answer, showAnswer}, index) => {
+          return (<QuestionAnswer
+            question={question}
+            answer={answer}
+            showAnswer={showAnswer}
+            onClick={onQuestionClick}
+            index={index}
+          />)
+        })}
       </div>
 
 
